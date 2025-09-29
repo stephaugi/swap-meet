897f61cd807759beb540986bd7ad4d55f146f9f1
Stephanie Lin <slin.animation@gmail.com>
2025-09-29 14:50:09 -0400

    Adds vendor class, __contains__, add and remove methods. Adds new test to test_wave_01.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,28 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = [] if inventory is None else inventory
+
+    def __contains__(self, item):
+        return self.lower <= item <= self.upper
+
+    def add(self, item):
+        '''
+        Adds the item to inventory
+        parameter: item
+        return: item added
+        '''
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        '''
+        Removes the item from the inventory
+        parameter: item
+        return: item removed
+        '''
+
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
+    
