71a02dca473cbbf1dc3d18859bb4fc8ac6baee17
Ekaterna <edbelova@outlook.com>
2025-10-02 13:11:39 -0700

    Checked that code is following PEP 8/PEP 257 conventions

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,28 +1,34 @@
 import uuid
 class Item:
-    def __init__(self, id = None, condition=0):
+    def __init__(self, id=None, condition=0):
         self.condition = condition
         self.id = id if id is not None else uuid.uuid4().int
 
 
     def __str__(self):
-        '''
+        """
         Converts item to string using str().
         returns: "An object of type Item with id <id value>."
         Operator overloading
-        '''
+        """
         category = self.get_category()
         return f"An object of type {category} with id {self.id}."
 
     
     def get_category(self):
-        '''
+        """
         Return a string holding the name of the class
         return name_of_class(str)
-        '''
+        """
         return "Item"
 
     def condition_description(self):
+        """
+        Return a human-readable description of the item's condition.
+
+        Returns:
+            str: A message describing the condition.
+        """
         if self.condition <= 1:
             return "Yikes. Don't wanna touch that."
         elif self.condition >= 3:
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -6,20 +6,20 @@ class Vendor:
         return self.lower <= item <= self.upper
 
     def add(self, item):
-        '''
+        """
         Adds the item to inventory
-        parameter: item
-        return: item added
-        '''
+        Args: item
+        Returns: item added
+        """
         self.inventory.append(item)
         return item
     
     def remove(self, item):
-        '''
+        """
         Removes the item from the inventory
-        parameter: item
-        return: item removed
-        '''
+        Args: item
+        Returns: item removed
+        """
 
         if item not in self.inventory:
             return False
@@ -27,29 +27,29 @@ class Vendor:
         return item
     
     def get_by_id(self, item_id):
-        '''
+        """
         Returns the item with the matching id from inventory
-        parameter: item_id (int)
-        return: item with the matching id or None if not found
-        '''
+        Args: item_id (int)
+        Returns: item with the matching id or None if not found
+        """
         for item in self.inventory:
             if item.id == item_id:
                 return item
         return None
     
     def swap_items(self, other_vendor, my_item, their_item):
-        '''
-        parameters: 3 arguments. 
+        """
+        Args: 3 arguments. 
         instance of another vendor(other_vendor)
         instance of item(my_item)
         instance of item(their_item)
 
-        return: True
+        Returns: True
         edgecase: if this vendor's inventory doesn't contain my_item
         the friend's inventory doesn't contain their_item, the method
         returns False
 
-        '''
+        """
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
         
@@ -73,14 +73,14 @@ class Vendor:
 
     def swap_first_item(self, other_vendor):
 
-        '''
+        """
         Swaps first item in this inventory and friend's inventory.
 
-        parameters: instance of another Vendor(other_vendor)
+        Args: instance of another Vendor(other_vendor)
 
-        returns: True if successful
+        Returns: True if successful
         False if either have empty inventory
-        '''
+        """
         
         if not self.inventory or not other_vendor.inventory:
             return False
@@ -88,34 +88,31 @@ class Vendor:
         my_first_item = self.inventory[0]
         their_first_item = other_vendor.inventory[0]
 
-        self.swap_items(other_vendor, my_first_item, their_first_item)
-
-        return True
+        return self.swap_items(other_vendor, my_first_item, their_first_item)
     
     def get_by_category(self, category):
-        '''
+        """
         gets objects in inventory by category
-        parameter: category(string)
-        returns: list
+        Args: category(string)
+        Returns: list
         edge case: if no item matches, return empty list []
-        '''
+        """
 
-        return [item for item in self.inventory if item.get_category()== category]
+        return [item for item in self.inventory if item.get_category() == category]
     
     def get_best_by_category(self, category):
-        '''
+        """
         get the item with the best condition in a certain category.
-        parameter: category(string)
-        returns: single item
+        Args: category(string)
+        Returns: single item
         edge case: if no item matches category, return None
-        '''
-        list_of_items_matched_category = self.get_by_category(category)
-        
-        if len(list_of_items_matched_category)==0:
+        """
+        items_in_category = self.get_by_category(category)
+        if len(items_in_category) == 0:
             return None
 
-        best_by_category = list_of_items_matched_category[0]
-        for object in list_of_items_matched_category[1:]:
+        best_by_category = items_in_category[0]
+        for object in items_in_category[1:]:
             if best_by_category.condition < object.condition:
                 best_by_category = object
         return best_by_category
@@ -138,12 +135,12 @@ class Vendor:
     #     return best_item
 
     def swap_best_by_category(self, other_vendor, my_priority, their_priority):
-        '''
+        """
         gets objects in inventory by category
-        parameter: category(string)
-        returns: list
+        Args: category(string)
+        Returns: list
         edge case: if no item matches, return empty list []
-        '''
+        """
 
         my_best_item = self.get_best_by_category(their_priority)
         their_best_item = other_vendor.get_best_by_category(my_priority)
